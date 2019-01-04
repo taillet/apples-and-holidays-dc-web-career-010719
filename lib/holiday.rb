5fd1c2fbedc7320a2c72bebb2690d6518e5b557a
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 01:15:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,4 +92,12 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        return holiday
+      end
+    end
+  end
+
 end
