cb92379e2e3f27f3d55634f82f01c01910a2397e
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 01:00:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    capitalized = season.capitalize!
+    capitalized = season.capitalize
     puts "#{capitalized}:"
     holidays.each do |holiday, supplies|
       array = holiday.split
