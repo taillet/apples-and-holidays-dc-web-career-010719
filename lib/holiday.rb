e2288b1e49451d9a65269fe19b3fd5965ce4d8ab
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 00:53:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash.each do |seezon, holiday|
     holiday.each do |holi, supplies|
       if seezon == season
-        
+
       end
   end
 
@@ -57,6 +57,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+
+  holiday_hash[:winter].values.flatten
 
 end
 
