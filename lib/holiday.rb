0b779a520364f7a29e1ccdf6de8fbe1fb5df75a1
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 00:52:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,6 +46,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash.each do |seezon, holiday|
     holiday.each do |holi, supplies|
       if seezon == season
+        
       end
   end
 
